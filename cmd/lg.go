f1f7c058b30c4556f4c048777b3c74f4a5d2c820
qi xu <qi.xu@autronicafire.no>
2022-03-07 13:47:25 +0100

    wip

--- cmd/lg.go
+++ cmd/lg.go
@@ -2,4 +2,6 @@ package main
 
 import "github.com/ebobo/logging_lab5e_go/pkg/logging"
 
-var lg = logging.Get().Named("gwsim").Sugar()
+// display package name also
+// var lg = logging.Get().Named("main").Sugar()
+var lg = logging.Get().Sugar()
--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,21 @@
 package main
 
+import (
+	"errors"
+
+	"github.com/ebobo/utilities_go/greeting"
+)
+
+var ErrDBDoesNotExist = errors.New("database does not exist")
+
 func main() {
+	greeting.Foreword("Logging Package from Lab5e")
+
 	lg.Info("log info")
+
+	lg.Error("log error")
+
+	// lg.Fatal("log fatal")
+	lg.Infof("log infof %v", ErrDBDoesNotExist)
 
 }
